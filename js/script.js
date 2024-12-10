df5511a1b9ee91fd5963452cc3353f55e60b687a
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-10 10:37:20 +0100

    function js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 // Dom element
-const imgLampElm =document.getElementById("lamp")
+const imgLampOffElm =document.getElementById("lampOff")
+const imgLampOnElm = document.getElementById ("lampOn")
 const buttonElm = document.getElementById("btn")
 
+let isLampOn = false;
 // dom events
 buttonElm.addEventListener("click", function() {
-    imgLampElm.src = 'img/yellow_lamp.png'
-    imgLampElm.alt = 'accesa'
+// inverto il valore diisLampOn 
+isLampOn = !isLampOn;
+
+    if (isLampOn) {
+        imgLampOnElm.classList.remove('d-none')
+        imgLampOffElm.classList.add('d-none')
+        buttonElm.innerHTML = "Spegni"
+        // lampadina accesa
+    } else {
+        imgLampOnElm.classList.add('d-none')
+        imgLampOffElm.classList.remove('d-none')
+        buttonElm.innerHTML = "Accendi"
+        // lampadina spenta
+    }
 });
